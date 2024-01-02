6c8a2d82c8c8bdd067260512b75b16a68cea8329
partypankes <147542459+partypankes@users.noreply.github.com>
2024-01-02 16:03:46 +0100

    removal

--- login/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$hash = '$2y$10$nz4RQWWi31D6aajNZemIpupm5ATlXoWoBudRWcO7rFSIUPspZDgk6';
-echo password_verify("ciao",$hash);
-if(password_verify("ciao",$hash)) {
-    echo "password giusta";
-}
