7ae6641dbb5300b0e4b330575409180e91c1508f
nelloodeluca <nelloodeluca@gmail.com>
2024-01-07 19:12:03 +0100

    Definizione css globale
    Meglio applicare questo a tutti i file, per aumentare la responsive del sito

--- aboutUs/AboutUs.php
+++ aboutUs/AboutUs.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>About Us - SapurEat</title>
+    <link rel="stylesheet" href="../global.css">
   <link rel="stylesheet" href="aboutUs.css">
 </head>
 <body>
