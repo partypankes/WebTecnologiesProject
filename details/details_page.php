6f6084ae1bfec7cab500d46052960228876fcee8
raffaelefesta19 <raffaelefesta19@gmail.com>
2024-01-13 17:03:21 +0100

    header update

---
+++ details/details_page.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Dettaglio Ricetta</title>
+</head>
+<body>
+<?php
+include('../header/headerUtenteRegistrato.html');
+?>
+</body>
+</html>
