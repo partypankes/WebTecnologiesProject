82c7f812f0b85d3423934d1f396f303565d9bb4f
partypankes <147542459+partypankes@users.noreply.github.com>
2024-01-16 11:56:16 +0100

    Update

--- details/details_query.php
+++ details/details_query.php
@@ -4,13 +4,13 @@ $section = $_GET['section'] ?? '';
 if(isset($db)) {
     switch ($section) {
         case 'ingredienti':
-            $query = "SELECT i.ingrediente_name, ri.quantita FROM ingrediente i INNER JOIN ricetta_ingrediente ri ON i.id = ri.ingrediente WHERE ri.ricetta = 4";
+            $query = "SELECT i.ingrediente_name, ri.quantita, ri.misurazione FROM ingrediente i INNER JOIN ricetta_ingrediente ri ON i.id = ri.ingrediente WHERE ri.ricetta = 4";
             $result = pg_query($db, $query);
             if ($result) {
-                $tableHTML = "<table border='1' id='tabella-ingredienti'><tr><th>Quantitá</th><th>Nome Ingrediente</th></tr>";
+                $tableHTML = "<table border='1' id='tabella-ingredienti'><tr><th>Quantitá</th><th>Misurazione</th><th>Nome Ingrediente</th></tr>";
                 $i = 1;
                 while ($row = pg_fetch_assoc($result)) {
-                    $tableHTML .= "<tr><td>".$row['quantita']."</td><td>".$row['ingrediente_name']."</td></tr>";
+                    $tableHTML .= "<tr><td>".$row['quantita']."</td><td>".$row['misurazione']."</td><td>".$row['ingrediente_name']."</td></tr>";
                 }
                 $tableHTML .= "</table>";
                 pg_close($db);
