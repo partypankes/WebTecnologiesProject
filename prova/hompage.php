6bc9fb356e087926ca12cc839b384412be865040
raffaelefesta19 <raffaelefesta19@gmail.com>
2024-02-08 17:56:51 +0100

    prova search bar

---
+++ prova/hompage.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SapurEat</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
+</head>
+<body>
+
+<div class="box">
+    <input type="text" placeholder="Trova la tua ispirazione...">
+    <button type="submit" aria-label="Cerca"><i class="fas fa-search"></i></button>
+</div>
+
+<script src="script.js"></script>
+</body>
+</html>
