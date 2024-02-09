bbbdaaa6d3ebb24f8d22b0935babed90fa67c232
raffaelefesta19 <raffaelefesta19@gmail.com>
2024-02-09 12:30:22 +0100

    add global css

--- prova/hompage.php
+++ prova/hompage.php
@@ -4,15 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SapurEat</title>
+    <link rel="stylesheet" href="../global.css">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
 </head>
 <body>
+<section id="first">
+    <div class="box">
+        <input type="text" placeholder="Trova la tua ispirazione...">
+        <button type="submit" aria-label="Cerca"><i class="fas fa-search"></i></button>
+    </div>
+</section>
 
-<div class="box">
-    <input type="text" placeholder="Trova la tua ispirazione...">
-    <button type="submit" aria-label="Cerca"><i class="fas fa-search"></i></button>
-</div>
 
 <script src="script.js"></script>
 </body>
