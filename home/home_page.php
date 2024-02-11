bdb038ab123d4dcd0026d6ff2200b0cedaff5327
nelloodeluca <nelloodeluca@gmail.com>
2024-02-11 17:57:17 +0100

    header finito
    da cambiare solo colori e piccoli aggiusti grafici

--- home/home_page.php
+++ home/home_page.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SapurEat - Homepage</title>
-    <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="../global.css">
+    <link rel="stylesheet" href="../home/home.css">
 </head>
 <body>
 
@@ -12,14 +13,14 @@
         //tipologia header differente per utente registato e per non registrato
         include('../_header/mainHeader.html');
         ?>
-    
-    <main>
+
         <section class="banner-logged">
             <?php
             //da visualizzare se è il primo acceso dell'utente non registrato o non è registrato
             include('../home/banner_home/banner_home.html');
             ?>
         </section>
+    <main>
         <section class="ricette-home">
             <div class="ricette">Sient</div>
         </section>
