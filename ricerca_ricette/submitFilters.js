ab2d3c2e2cd4781cb1f01a79bf6655d64869e50e
partypankes <147542459+partypankes@users.noreply.github.com>
2024-02-12 20:16:18 +0100

    Update on js

--- ricerca_ricette/submitFilters.js
+++ ricerca_ricette/submitFilters.js
@@ -1,20 +1,41 @@
-let check = document.querySelectorAll('.filtro');
-console.log(check);
+let tempo = document.getElementsByName('tempo');
+
 
 let portata = document.getElementsByName('portata');
 console.log(portata);
 
 function submitFilter(){
-    var checked_box;
-    portata_box = [];
-    tempo_box = [];
+    var portata_box;
+    var tempo_box;
+
+
 
     portata.forEach(function(checkbox) {
         if(checkbox.checked) {
-            checked_box.push(checkbox.value);
+            portata_box = checkbox.value;
         }
     });
 
+    tempo.forEach(function (checkbox){
+       if(checkbox.checked) {
+           tempo_box = checkbox.value;
+       }
+    });
 
-    console.log(checked_box);
+    var query = "SELECT * FROM ricetta WHERE tempo_preparazione = <" + tempo_box + "AND portata = " + portata_box + ";";
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST",'query_ricerca.php', true);
+
+
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            console.log(xhr.responseText);
+        }
+    };
+
+
+    xhr.send(query);
+
 }
