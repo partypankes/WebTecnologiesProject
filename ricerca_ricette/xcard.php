cfd2f22926d116d262358b87e754958ece0721d6
nelloodeluca <nelloodeluca@gmail.com>
2024-02-13 18:45:55 +0100

    bug header risolto

--- ricerca_ricette/xcard.php
+++ ricerca_ricette/xcard.php
@@ -10,17 +10,17 @@ if(isset($db) && isset($sql) && isset($parametri)) {
             $imageData = pg_unescape_bytea($row['banner']);
             echo '<div class="xcard"> 
                     <img class="banner-xcard" src="data:image/jpeg;base64,'. base64_encode($imageData) .'"/>'.
-                '<div class="info-xcard">
-                    <a>' . $row['titolo'] . '</a>
-                    <div class="sub-xcard">
-                    <p>by <span>' . $row['utente'] . '</span></p>
-                    <div class="extra-xcard">
-                    <p><span id="minutes">'. $row['tempo_preparazione'] . '</span><i class="fa-regular fa-clock"></i></p>
-                    <p><span id="voto">xxxxxxx</span>/5<i class="fa-solid fa-star"></i></p>
+                    '<div class="info-xcard">
+                        <a>' . $row['titolo'] . '</a>
+                        <div class="sub-xcard">
+                            <p>by <span>' . $row['utente'] . '</span></p>
+                            <div class="extra-xcard">
+                                <p><span id="minutes">'. $row['tempo_preparazione'] . '</span><i class="fa-regular fa-clock"></i></p>
+                                <p><span id="voto">'. $row['voto'] . '</span>/5<i class="fa-solid fa-star"></i></p>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            </div>';
+                </div>';
         }
 
     }
