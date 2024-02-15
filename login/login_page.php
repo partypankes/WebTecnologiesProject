e3bbc5648a8d5bbae121d9fbd0dc06f934ad0380
nelloodeluca <nelloodeluca@gmail.com>
2024-02-15 15:37:38 +0100

    global.css + details

--- login/login_page.php
+++ login/login_page.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="it">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../global.css">
-    <link rel="stylesheet" href="login_page.css">
+    <link rel="stylesheet" href="/global.css">
+    <link rel="stylesheet" href="/login/login_page.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" rel="stylesheet">
     <title>Login Page</title>
 </head>
@@ -52,6 +52,6 @@
     </div>
 
 </div>
-<script src="login_validation.js"></script>
+<script src="/login/login_validation.js"></script>
 </body>
 </html>
