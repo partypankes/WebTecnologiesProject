30b4c187f1194c0fbf82b46db3b0b877a76d0ba3
partypankes <147542459+partypankes@users.noreply.github.com>
2024-02-17 15:05:59 +0100

    Meccanismo media voto

---
+++ core/media_voto.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+if(isset($db) && isset($id)) {
+
+    $sql = "SELECT voto FROM recensione WHERE ricetta = $1";
+    $result = pg_query_params($db,$sql,array($id));
+    $somma = 0;
+    $cont = 0;
+    if($result) {
+        while($row = pg_fetch_assoc($result)) {
+            $somma += $row['voto'];
+            $cont++;
+        }
+        $somma /= $cont;
+
+        $sql_voto_ricetta = "UPDATE ricetta SET voto = $1 WHERE id = $2";
+        pg_query_params($db,$sql_voto_ricetta,array(floor($somma),$id));
+    }
+
+}
