9dedfb0e095f80dceec45e28b82ce7b0fbca350a
partypankes <147542459+partypankes@users.noreply.github.com>
2024-02-17 19:34:18 +0100

    Ricette Homepage

---
+++ core/ricette_homepage.php
@@ -0,0 +1,28 @@
+<?php
+
+include 'core/dbconnection.php';
+
+if(isset($db)) {
+    $sql = "SELECT * FROM ricetta ORDER BY voto DESC LIMIT 5;";
+    $result = pg_query($db,$sql);
+    while($row = pg_fetch_assoc($result)) {
+        $imageData = pg_unescape_bytea($row['banner']);
+        echo  '<div class="ycard">
+                    <div class="y-banner">
+                        <img class="banner-ycard" src="data:image/jpeg;base64,'. base64_encode($imageData)  .'"/>
+                                <p id="user-ycard">by <span>' . $row['utente'] . '</span></p>
+                                </div>
+                                <div class="info-ycard">
+                                <a href="dettagli_ricetta.php?id=' . $row['id'] . '">'.$row['titolo'] .'</a>
+                                <div class="extra-ycard">
+                                    <p><span id="minutes">'. $row['tempo_preparazione'] .'</span><i class="fa-regular fa-clock"></i>
+                                    </p>
+                                    <p><span id="voto">'. $row['voto'] . '</span>/5<i class="fa-solid fa-star"></i></p>
+                                </div>
+
+                            </div>
+                        </div>';
+
+    }
+
+}
