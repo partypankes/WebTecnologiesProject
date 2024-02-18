e60ff1e28b7e073875ee35b17da68c25c56473b5
raffaelefesta19 <raffaelefesta19@gmail.com>
2024-02-18 17:46:30 +0100

    add commenti in js animazioni user_page

--- assets/js/user_page/user_page_script.js
+++ assets/js/user_page/user_page_script.js
@@ -1,37 +1,44 @@
+// Aspetta il completo caricamento del DOM prima di eseguire lo script
 document.addEventListener('DOMContentLoaded', (event) => {
+    // Seleziona tutte le sezioni e i link nella sidebar per la gestione della navigazione
     const sections = document.querySelectorAll('.section_show');
     const sidebarLinks = document.querySelectorAll('.sidebar a');
+    // Funzione per nascondere tutte le sezioni e rimuovere la classe 'active' dai link della sidebar
     function resetActiveSection() {
+        // Nasconde ogni sezione
         sections.forEach(section => {
             section.style.display = 'none';
         });
+        // Rimuove la classe 'active' da ogni link nella sidebar
         sidebarLinks.forEach(link => {
             link.classList.remove('active');
         });
     }
 
-    // Imposta la sezione Account come attiva all'avvio
+    // Imposta la sezione Account come visibile all'avvio
     document.getElementById('account').style.display = 'block';
+    // Imposta il primo link nella sidebar come attivo all'avvio
     document.querySelector('.sidebar a').classList.add('active');
 
+    // Aggiunge un listener per ogni link nella sidebar per la navigazione
     sidebarLinks.forEach((link, index) => {
         link.addEventListener('click', (e) => {
-            e.preventDefault();
+            e.preventDefault(); // Previene il comportamento di default del link
 
-            resetActiveSection();
-
-
-            link.classList.add('active');
+            resetActiveSection(); // Resetta la sezione attiva e rimuove la classe 'active'
 
 
-            sections[index].style.display = 'block';
+            link.classList.add('active'); // Imposta il link cliccato come attivo
+
+
+            sections[index].style.display = 'block'; // Mostra la sezione corrispondente al link cliccato
         });
     });
 
     //Ajax Form Anagrafica
     var form = document.getElementById('anagrafica');
     form.addEventListener('submit', function(e) {
-        e.preventDefault();
+        e.preventDefault(); 
         var formData = new FormData(this);
         let xhr = new XMLHttpRequest();
 
