5f7346038cd8bfbf2a147c61edcfb2af4bf0efa9
partypankes <147542459+partypankes@users.noreply.github.com>
2024-02-18 12:27:43 +0100

    Sesso query

--- core/user_page/fetch_dati_utente.php
+++ core/user_page/fetch_dati_utente.php
@@ -12,13 +12,19 @@ $username = $_SESSION['username'];
     $result = pg_query_params($db,$sql,array($username));
     if($result) {
         $row = pg_fetch_assoc($result);
-
+        if($row['sesso'] == 'M') {
+            $sesso = "Male";
+        } else if ($row['sesso'] == 'F') {
+            $sesso = "Female";
+        } else {
+            $sesso = "Other";
+        }
         echo '<div class="user-profile">
                     <p class="username">Username: <span>' . $row['username'] . '</span></p>
                     <p class="name">Nome: <span>'. $row['nome'] . '</span></p>
                     <p class="surname">Cognome: <span>'. $row['cognome'] .'</span></p>
                     <p class="birthdate">Data di nascita: <span>'. $row['data_di_nascita'] .'</span></p>
-                    <p class="gender">Sesso: <span>'. $row['sesso'].'</span></p>
+                    <p class="gender">Sesso: <span>'. $sesso .'</span></p>
                     <p class="email">Email: <span>'. $row['email']. '</span></p>
                 </div>';
 
